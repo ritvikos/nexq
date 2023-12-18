5a8b565a8d87432ebd65156b89cb246502b0e0de
Ritvik-UX <ritvikux@gmail.com>
2023-12-18 11:07:45 +0530

    chore: experiment with design pattern

--- nerfq/src/strategy.rs
+++ nerfq/src/strategy.rs
@@ -1,9 +1,11 @@
+use nanorand::{Rng, WyRand};
 use std::sync::atomic::{AtomicUsize, Ordering};
 
 #[derive(Debug)]
 pub enum Strategy {
-    RoundRobin { idx: AtomicUsize },
-    HashBased { hasher: Hasher, key: String },
+    RoundRobin(AtomicUsize),
+    HashBased(Hasher),
+    Random,
 }
 
 #[derive(Debug, Clone)]
@@ -16,31 +18,27 @@ pub enum Hasher {
 impl Clone for Strategy {
     fn clone(&self) -> Self {
         match self {
-            Self::RoundRobin { idx } => Self::RoundRobin {
-                idx: AtomicUsize::new(idx.load(Ordering::Acquire)),
-            },
-
-            Self::HashBased { hasher, key } => Self::HashBased {
-                hasher: hasher.clone(),
-                key: key.clone(),
-            },
+            Self::RoundRobin(idx) => {
+                Self::RoundRobin(AtomicUsize::new(idx.load(Ordering::Acquire))).clone()
+            }
+            Self::HashBased(hasher) => Self::HashBased(hasher.clone()),
+            Self::Random => self.clone(),
         }
     }
 }
 
 impl Default for Strategy {
     fn default() -> Self {
-        Self::RoundRobin {
-            idx: AtomicUsize::default(),
-        }
+        Self::RoundRobin(AtomicUsize::default())
     }
 }
 
 impl Strategy {
-    pub fn rotate(&self, len: &usize) -> usize {
+    pub fn rotate(&self, len: usize) -> usize {
         match self {
-            Self::RoundRobin { idx } => idx.fetch_add(1, Ordering::AcqRel) % len,
-            Self::HashBased { hasher, key } => todo!(),
+            Self::RoundRobin(idx) => idx.fetch_add(1, Ordering::AcqRel) % len,
+            Self::HashBased(hasher) => todo!(),
+            Self::Random => WyRand::new().generate_range(0..=len),
         }
     }
 }
